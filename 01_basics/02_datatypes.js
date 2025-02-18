85974b0c694ca437e23b7b962a2e20e6bd863832
shashanksingh945 <143628773+shashanksingh945@users.noreply.github.com>
2025-02-18 19:39:42 +0000

    variable

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treate all JS code as newer versior\\
+
+// alert(3+3)  //we r using m=node.js not browser
+
+console.log(3+3)
+
+console.log("Shashank")
+
+let name="Shashank"
+let age=21
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>" "
+//boolean=> true/false
+//null=>standalone value
+//undefine=>
+//symbole =>unique
+
+//object
+
+console.log(typeof none);   //none
+console.log(typeof undefined);   //undefine
