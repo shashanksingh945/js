c3d5d35d2d92a391d9a2917be8f26e6ac1a4e249
shashanksingh945 <143628773+shashanksingh945@users.noreply.github.com>
2025-07-11 11:01:31 +0000

    array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,4 +1,15 @@
 const marvelHeroes = ["thor","ironman","spiderman"]
 const dcHeroes = ["superman","flash","batman"]
-marvelHeroes.push(dcHeroes)
-console.log(marvelHeroes);
+
+// marvelHeroes.push(dcHeroes)
+// console.log(marvelHeroes);
+
+// const allHeroes= marvelHeroes.concat(dcHeroes);
+// console.log(allHeroes)
+
+const allnewHeroes = [...marvelHeroes, ...dcHeroes]
+// console.log(allnewHeroes)
+
+const a2= [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const ra2 = a2.flat(Infinity);
+consol
